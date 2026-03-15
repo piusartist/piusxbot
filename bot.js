7b4cf57315f7173e200bf7b391dcdc4c20984bb4
Pius Murimi <piusartist0@gmail.com>
2026-03-15 11:28:26 +0300

    Fix Puppeteer Chrome error for Render

--- bot.js
+++ bot.js
@@ -2,7 +2,20 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process', // <- important for Render
+            '--disable-gpu'
+        ]
+    }
 });
 
 let autoReply = false;
